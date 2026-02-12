d039d035cab9027fdc0914a5b250e185de02877c
Yogesh Mahawar <yogeshfreelancer78@gmail.com>
2026-02-12 12:11:59 +0530

    Fix PascalCase component naming for production build

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,22 @@ import Contracting from "./Contracting/Contracting";
 import About from "./About";
 import Contact from "./Contact";
 import Testimonials from "./Testimonials";
-import Interior_Design from "./Contracting/Interior_Design";
-import Home_Living_Design from "./Contracting/Home_Living_Design";
-import Office_Design from "./Contracting/Office_Design";
+import InteriorDesign from "./Contracting/Interior_Design";
+import HomeLivingDesign from "./Contracting/Home_Living_Design";
+import OfficeDesign from "./Contracting/Office_Design";
 import Chairs from "./Shop/Chairs";
 import Sofas from "./Shop/Sofas";
 import Doors from "./Shop/Doors";
 import Windows from "./Shop/Windows";
 import Almiras from "./Shop/Almiras";
 import Bads from "./Shop/Bads";
-import Dinning_Table from "./Shop/Dinning_Table";
-import Shoe_Racks from "./Shop/Shoe_Racks";
-import Study_Table from "./Shop/Study_Table";
-import Wooden_ceiling from "./Shop/Wooden_celing";
-import Furniture_Reparation from "./Contracting/Furniture_Reparation";
-import Wedding_furniture from "./Contracting/Wedding_furniture";
-import Design_consultation from "./Contracting/Design_consultation";
+import DinningTable from "./Shop/Dinning_Table";
+import ShoeRacks from "./Shop/Shoe_Racks";
+import StudyTable from "./Shop/Study_Table";
+import Woodenceiling from "./Shop/Wooden_celing";
+import FurnitureReparation from "./Contracting/Furniture_Reparation";
+import Weddingfurniture from "./Contracting/Wedding_furniture";
+import Designconsultation from "./Contracting/Design_consultation";
 import { useEffect } from "react";
 
 function App() {
@@ -41,37 +41,34 @@ function App() {
       <Route path="/about-us" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/testimonials" element={<Testimonials />} />
-      <Route
-        path="/contracting/interior-design"
-        element={<Interior_Design />}
-      />
+      <Route path="/contracting/interior-design" element={<InteriorDesign />} />
       <Route
         path="/contracting/home-living-design"
-        element={<Home_Living_Design />}
+        element={<HomeLivingDesign />}
       />
       <Route
         path="/contracting/furniture-reparation"
-        element={<Furniture_Reparation />}
+        element={<FurnitureReparation />}
       />
       <Route
         path="/contracting/wedding-furniture"
-        element={<Wedding_furniture />}
+        element={<Weddingfurniture />}
       />
       <Route
         path="/contracting/design-consultation"
-        element={<Design_consultation />}
+        element={<Designconsultation />}
       />
-      <Route path="/contracting/office-design" element={<Office_Design />} />
+      <Route path="/contracting/office-design" element={<OfficeDesign />} />
       <Route path="/shop/chairs" element={<Chairs />} />
       <Route path="/shop/sofas" element={<Sofas />} />
       <Route path="/shop/doors" element={<Doors />} />
       <Route path="/shop/windows" element={<Windows />} />
       <Route path="/shop/almiras" element={<Almiras />} />
       <Route path="/shop/bads" element={<Bads />} />
-      <Route path="/shop/dinning-table" element={<Dinning_Table />} />
-      <Route path="/shop/shoe-racks" element={<Shoe_Racks />} />
-      <Route path="/shop/study-table" element={<Study_Table />} />
-      <Route path="/shop/wooden-ceiling" element={<Wooden_ceiling />} />
+      <Route path="/shop/dinning-table" element={<DinningTable />} />
+      <Route path="/shop/shoe-racks" element={<ShoeRacks />} />
+      <Route path="/shop/study-table" element={<StudyTable />} />
+      <Route path="/shop/wooden-ceiling" element={<Woodenceiling />} />
     </Routes>
   );
 }
