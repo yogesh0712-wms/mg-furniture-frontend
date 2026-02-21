cd7b74fe35a2e2a2308bb26f35b397482130e24b
Yogesh Mahawar <yogeshfreelancer78@gmail.com>
2026-02-21 21:38:40 +0530

    Added SEO title and meta description to Shop page

--- src/Shop/Shop.jsx
+++ src/Shop/Shop.jsx
@@ -4,19 +4,6 @@ import ShopHeroSection from "./ShopHeroSection";
 import Products, { ProductsQuality } from "../Products";
 import { useEffect } from "react";
 
-useEffect(() => {
-  document.title =
-    "Furniture Store in Jaipur | Custom Wooden Furniture | MG Furniture";
-
-  const metaDescription = document.querySelector('meta[name="description"]');
-  if (metaDescription) {
-    metaDescription.setAttribute(
-      "content",
-      "Explore custom wooden furniture in Jaipur including sofas, beds, dining tables, wardrobes and more. Designed for durability and crafted for real homes by MG Furniture.",
-    );
-  }
-}, []);
-
 const popularProducts = [
   {
     image: "image_collection/shop/chairs - Copy.png",
@@ -83,6 +70,19 @@ const popularProducts = [
 ];
 
 const Shop = () => {
+  useEffect(() => {
+    document.title =
+      "Furniture Store in Jaipur | Custom Wooden Furniture | MG Furniture";
+
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute(
+        "content",
+        "Explore custom wooden furniture in Jaipur including sofas, beds, dining tables, wardrobes and more. Designed for durability and crafted for real homes by MG Furniture.",
+      );
+    }
+  }, []);
+
   return (
     <div>
       <Header />
