c9b4581edc91f65ffbe0afdf6ae33b1370605c62
Yogesh Mahawar <yogeshfreelancer78@gmail.com>
2026-02-28 15:47:29 +0530

    Enabled right border in product quality section

--- src/Products.jsx
+++ src/Products.jsx
@@ -92,7 +92,7 @@ const ProductsQuality = () => {
   return (
     <div>
       <section className="grid grid-cols-1 gap-6 px-4 lg:flex lg:px-0">
-        <div className="border-2 border-r-0 w-full lg:w-96 h-48 flex gap-6">
+        <div className="border-2  w-full lg:w-96 h-48 flex gap-6">
           <div className="pt-10 pl-6 lg:pl-10">
             <Star size={36} strokeWidth={1.5} color="#D6D6B5" />
           </div>
@@ -105,7 +105,7 @@ const ProductsQuality = () => {
           </div>
         </div>
 
-        <div className="border-2 border-r-0 w-full lg:w-96 h-48 flex gap-6">
+        <div className="border-2 w-full lg:w-96 h-48 flex gap-6">
           <div className="pt-10 pl-6 lg:pl-10">
             <Tag size={36} strokeWidth={1.5} color="#D6D6B5" />
           </div>
@@ -118,7 +118,7 @@ const ProductsQuality = () => {
           </div>
         </div>
 
-        <div className="border-2 border-r-0 w-full lg:w-96 h-48 flex gap-6">
+        <div className="border-2 w-full lg:w-96 h-48 flex gap-6">
           <div className="pt-10 pl-6 lg:pl-10">
             <ShoppingCart size={36} strokeWidth={1.5} color="#D6D6B5" />
           </div>
@@ -131,7 +131,7 @@ const ProductsQuality = () => {
           </div>
         </div>
 
-        <div className="border-2 border-r-0 w-full lg:w-96 h-48 flex gap-6">
+        <div className="border-2 w-full lg:w-96 h-48 flex gap-6">
           <div className="pt-10 pl-6 lg:pl-10">
             <Lock size={36} strokeWidth={1.5} color="#D6D6B5" />
           </div>
